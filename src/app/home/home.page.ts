127791c28e0f8351b2d2b87f7ff83a381df8f386
benoitlebarbu <dolbeaubenoit@gmail.com>
2022-07-07 14:44:33 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  currentDate: string;
+  isOpen: boolean = false;
+  tasks: any[] = [];
+  newTask: string;
+  
+  constructor() {
+    const date = new Date();
+  this.currentDate = date.toLocaleDateString('fr-FR');
+  }
 
-  constructor() {}
+  addNewTask() {
+    var task = {
+      isChecked: false,
+      content : this.newTask
+    }
+    this.newTask = '';
+    this.tasks.push(task);
+  }
 
+  onCheck(event, i) {
+    this.tasks[i].isChecked = event.detail.checked;
+  }
+  deleteTask(i) {
+    this.tasks.splice(i, 1)
+  }
 }
